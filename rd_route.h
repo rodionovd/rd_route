7a87a3628abd9baa2606e45622656c56549e20dd
rodionovd <i.am.rodionovd@gmail.com>
2014-02-24 15:07:35 +0700

    Update in-code documentation in rd_route.h

--- rd_route.h
+++ rd_route.h
@@ -6,7 +6,27 @@
 
 #ifndef RD_ROUTE
     #define RD_ROUTE
+
+/**
+ * Override `function` to jump directly into `replacement` location. Caller can later
+ * access an original function's implementation via `original_ptr` (if passed).
+ * Note that `original_ptr` won't be equal to `function` due to remapping the latter
+ * into a different memory space.
+ *
+ * @param  function    pointer to a function to override;
+ * @param  replacement pointer to new implementation;
+ * @param  original    will be set to an address of the original implementation's copy;
+ *
+ * @return             KERN_SUCCESS if succeeded, or other value if failed
+ */
     int rd_route(void *function, void *replacement, void **original);
+/**
+ * Copy `function` implementation into another (first available) memory region.
+ * @param  function  pointer to a function to override;
+ * @param  duplicate will be set to an address of the function's implementation copy
+ *
+ * @return KERN_SUCCESS if succeeded, or other value if failed
+ */
     int rd_duplicate_function(void *function, void **duplicate);
 #endif
 
