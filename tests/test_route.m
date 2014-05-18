34b3ff9ce0ab2b24c9025887f99a191c816750f7
rodionovd <i.am.rodionovd@gmail.com>
2014-05-19 02:29:01 +0700

    Update test cases to save an original implementation of strerror() when routing

--- tests/test_route.m
+++ tests/test_route.m
@@ -6,10 +6,6 @@ extern int rd_route(void *function, void *replacement, void **original);
 	#define fprintf(file, format, ...)
 #endif
 
-
-static void* (*orig_strerror)(int) = NULL;
-static int   (*orig_computation)(int, int) = NULL;
-
 __attribute__((noinline))
 static char *my_strerror(int err)
 {
@@ -31,22 +27,25 @@ static int NSA_computation(int first, int second)
 
 int main(int argc, char const *argv[])
 {
-
-	orig_strerror = NULL;
+    char* (*orig_strerror)(int) = NULL;
 	int err = 2;
+    char *expected_string = strerror(err);
 	fprintf(stderr, "Original [strerror(%d)]: %s\n", err, strerror(err));
 
-	rd_route(strerror, my_strerror, NULL);
+	rd_route(strerror, my_strerror, (void **)&orig_strerror);
 
 	assert(0 == strcmp(strerror(err), my_strerror(err)));
+    assert(orig_strerror);
+    assert(0 == strcmp(expected_string, orig_strerror(err)));
 
 	fprintf(stderr, "Routed   [strerror(%d)]: %s\n", err, strerror(err));
+    fprintf(stderr, "Original [strerror(%d)]: %s\n", err, orig_strerror(err));
 	fprintf(stderr, "---------\n");
 
 
-	orig_computation = NULL;
+	int (*orig_computation)(int, int) = NULL;
 	int first = 3, second = 9;
-	int wanted_result = key_computation(first, second);
+	int expected_result = key_computation(first, second);
 
 	fprintf(stderr, "Original [key_computation(%d, %d)]: %d\n",
 		first, second, key_computation(first, second));
@@ -55,9 +54,9 @@ int main(int argc, char const *argv[])
 
 	assert(key_computation(first, second) == NSA_computation(first, second));
 	assert(orig_computation);
-	assert(wanted_result == orig_computation(first, second));
+	assert(expected_result == orig_computation(first, second));
 
-	fprintf(stderr, "Routed   [key_computation(%d, %d)]: %d (but must be == %d)\n",
+	fprintf(stderr, "Routed   [key_computation(%d, %d)]: %d (was %d)\n",
 		first, second, key_computation(first, second),
 		orig_computation(first, second));
 	fprintf(stderr, "---------\n");
