c9659f6f61ba1da04656ea7c80b4485b702c63a8
trueToastedCode <lennard2hp@gmail.com>
2023-10-25 21:19:11 +0200

    comply with W^X permission while patching memory

--- rd_route.c
+++ rd_route.c
@@ -340,22 +340,22 @@ static kern_return_t _patch_memory(void *address, mach_msg_type_number_t count, 
 	assert(new_bytes);
 
 	kern_return_t kr = 0;
-	kr = mach_vm_protect(mach_task_self(), (mach_vm_address_t)address, (mach_vm_size_t)count, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE | VM_PROT_COPY);
-	if (kr != KERN_SUCCESS) {
-		RDErrorLog("mach_vm_protect() failed with error: 0x%x", kr);
-		return (kr);
-	}
+	kr = mach_vm_protect(mach_task_self(), (mach_vm_address_t) address, (mach_vm_size_t) count, FALSE, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    if (kr != KERN_SUCCESS) {
+        RDErrorLog("mach_vm_protect() failed with error: 0x%x", kr);
+        return (kr);
+    }
 
-	kr = mach_vm_write(mach_task_self(), (mach_vm_address_t)address, (vm_offset_t)new_bytes, count);
-	if (kr != KERN_SUCCESS) {
-		RDErrorLog("mach_vm_write() failed with error: 0x%x", kr);
-		return (kr);
-	}
+    kr = mach_vm_write(mach_task_self(), (mach_vm_address_t) address, (vm_offset_t) new_bytes, count);
+    if (kr != KERN_SUCCESS) {
+        RDErrorLog("mach_vm_write() failed with error: 0x%x", kr);
+        return (kr);
+    }
 
-	kr = mach_vm_protect(mach_task_self(), (mach_vm_address_t)address, (mach_vm_size_t)count, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
-	if (kr != KERN_SUCCESS) {
-		RDErrorLog("mach_vm_protect() failed with error: 0x%x", kr);
-	}
+    kr = mach_vm_protect(mach_task_self(), (mach_vm_address_t) address, (mach_vm_size_t) count, FALSE, VM_PROT_READ | VM_PROT_EXECUTE);
+    if (kr != KERN_SUCCESS) {
+        RDErrorLog("mach_vm_protect() failed with error: 0x%x", kr);
+    }
 
 	return (kr);
 }
